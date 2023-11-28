43c77aabff1112a704a5cafc940a9112225a71e3
blindelectron <jump.leper@gmail.com>
2023-11-28 08:07:48 -0600

    Adds powershell configs.

--- links.bat
+++ links.bat
@@ -1,3 +1,5 @@
 mklink /J %appdata%\foobar2000-v2 foobar2000-v2 
 mklink /J %appdata%\ExplorerPatcher ExplorerPatcher
 mklink /J %appdata%\Rizonesoft Rizonesoft 
+mklink /J %userprofile%\Documents\WindowsPowerShell ps5
+mklink /J %userprofile%\Documents\PowerShell ps7
