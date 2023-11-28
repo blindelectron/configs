1159143b8581a5f20fe64dbe17c9c92d99b6dcf4
blindelectron <jump.leper@gmail.com>
2023-11-27 21:22:01 -0600

    add files.

--- links.bat
+++ links.bat
@@ -1,1 +1,3 @@
-mklink -J .config %userprofile%\.config
+mklink /J %appdata%\foobar2000-v2 foobar2000-v2 
+mklink /J %appdata%\ExplorerPatcher ExplorerPatcher
+mklink /J %appdata%\Rizonesoft Rizonesoft 
