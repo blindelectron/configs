bf41e8c0008bda9c28a8c424cd6016033b5c6305
blindelectron <jump.leper@gmail.com>
2023-12-16 11:17:27 -0600

    Adds windows terminal config file.

--- links.bat
+++ links.bat
@@ -3,3 +3,4 @@ mklink /J %appdata%\ExplorerPatcher ExplorerPatcher
 mklink /J %appdata%\Rizonesoft Rizonesoft 
 mklink /J %userprofile%\Documents\WindowsPowerShell ps5
 mklink /J %userprofile%\Documents\PowerShell ps7
+mklink /J %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState terminal
