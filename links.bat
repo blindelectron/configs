4aa5804a12301da07ec509cd84b5031381192aba
blindelectron <jump.leper@gmail.com>
2023-12-16 11:28:26 -0600

    Adds KeyOp configuration.

--- links.bat
+++ links.bat
@@ -4,3 +4,4 @@ mklink /J %appdata%\Rizonesoft Rizonesoft
 mklink /J %userprofile%\Documents\WindowsPowerShell ps5
 mklink /J %userprofile%\Documents\PowerShell ps7
 mklink /J %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState terminal
+mklink /J %userprofile%\portable\KeyOp\data keyop
